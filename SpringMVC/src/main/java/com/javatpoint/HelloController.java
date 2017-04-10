ba3e15b84f0189bd386773826cc1ac9d88cebb22
anand kumar <anandkmr994@gmail.com>
2017-04-11 00:40:51 +0530

    changes for aerospike

--- SpringMVC/src/main/java/com/javatpoint/HelloController.java
+++ SpringMVC/src/main/java/com/javatpoint/HelloController.java
@@ -1,5 +1,10 @@
 package com.javatpoint;
 
+import com.aerospike.client.Bin;
+import com.aerospike.client.Key;
+import com.aerospike.client.Record;
+import com.aerospike.client.policy.ClientPolicy;
+import com.aerospike.client.policy.Policy;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -9,6 +14,23 @@ public class HelloController {
 
 	@RequestMapping("/hello")
 	public ModelAndView mymethod(){
+
 		return new ModelAndView("hellopage","msg","Hello First Spring");
+	}
+
+	@RequestMapping("/aerospike/put")
+	public void putInAerospike(){
+		ClientPolicy policy = AerospikeInstance.getAerospikeClientPolicy();
+		Key key = new Key("test","experiment","name");
+		Bin bin = new Bin("name","anand");
+		AerospikeInstance.getClientInstance().add(policy.writePolicyDefault, key, bin);
+	}
+
+	@RequestMapping("/aerospike/get")
+	public ModelAndView getFromAerospike(){
+		ClientPolicy policy = AerospikeInstance.getAerospikeClientPolicy();
+		Key key = new Key("test","experiment","name");
+		Record record = AerospikeInstance.getClientInstance().get(policy.readPolicyDefault, key);
+		return new ModelAndView("hellopage","msg",record.toString());
 	}
 }
---
+++ SpringMVC/src/main/java/com/javatpoint/PropertiesContext.java
@@ -0,0 +1,59 @@
+package com.javatpoint;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.InputStream;
+import java.util.Properties;
+
+/**
+ * Created on 10/4/17.
+ */
+public class PropertiesContext {
+    private static final Logger LOG = LoggerFactory.getLogger(PropertiesContext.class);
+    private static PropertiesContext INSTANCE = new PropertiesContext();
+    private final Properties CONFIG_PROPERTIES;
+    //private final Properties PREFERENCES_VALUES_PROPERTIES;
+    //private final Properties OFFLINE_CLIENT_REF_VALUES_PROPERTIES;
+
+
+    private PropertiesContext() {
+        CONFIG_PROPERTIES = loadProperties("common-config.properties");
+        //PREFERENCES_VALUES_PROPERTIES=loadProperties("preference-values.properties");
+        //OFFLINE_CLIENT_REF_VALUES_PROPERTIES=loadProperties("offline-client-ref.properties");
+    }
+
+    private static Properties loadProperties(String url) {
+        Properties properties = new Properties();
+        try {
+            InputStream in = PropertiesContext.class.getClassLoader().getResourceAsStream(url);
+            properties.load(in);
+        } catch (Throwable t) {
+            LOG.error("Error in reading properties " + t.toString());
+        }
+        return properties;
+    }
+
+    public static final PropertiesContext getInstance() {
+        return INSTANCE;
+    }
+
+    public Properties getConfigProperties() {
+        return CONFIG_PROPERTIES;
+    }
+
+    public String getConfigProperty(String key) {
+        return CONFIG_PROPERTIES.getProperty(key);
+    }
+
+    /*
+    public static final Properties getPreferenceValuesProperties(){
+        return (Properties) getInstance().PREFERENCES_VALUES_PROPERTIES.clone();
+    }
+
+    public static final Properties getOfflineClientRefValuesProperties(){
+        return (Properties) getInstance().OFFLINE_CLIENT_REF_VALUES_PROPERTIES.clone();
+    }
+
+    */
+}
